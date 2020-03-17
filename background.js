b291f8b93515f45eb9012592b232e9a18202837a
TaoreX <62270834+TaoreX@users.noreply.github.com>
2020-03-17 10:16:49 +0800

    Add files via upload

---
+++ background.js
@@ -0,0 +1,7 @@
+//"https://api.ixiaowai.cn/mcapi/mcapi.php//表情包"
+var bg = [
+    "url(https://api.ixiaowai.cn/api/api.php)",
+    "url(http://www.dmoe.cc/random.php)"
+]
+
+document.body.style.backgroundImage=bg[parseInt(Math.random())];
